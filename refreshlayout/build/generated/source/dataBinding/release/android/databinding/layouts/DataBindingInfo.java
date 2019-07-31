6084a98b91818099d85329a1bd70103209bf571c
popularcloud <liushuzhiyt@163.com>
2019-07-31 16:08:48 +0800

    清除build文件

--- refreshlayout/build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="d4ad6b3f-4c65-4810-93d4-043e7e5a5641", modulePackage="cn.bingoogolapple.refreshlayout", sdkRoot="D:\\Android\\SDK",layoutInfoDir="F:\\lishenkeji_project_svn\\SHANXIU06042120\\refreshlayout\\build\\intermediates\\data-binding-info\\release",exportClassListTo="F:\\lishenkeji_project_svn\\SHANXIU06042120\\refreshlayout\\build\\intermediates\\data-binding-info\\release\\_generated.txt",isLibrary=true,minSdk=15,enableDebugLogs=false,printEncodedError=true)
-public class DataBindingInfo {}
